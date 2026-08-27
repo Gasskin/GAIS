187e94af131033af065eb893dc6911c66c2355b7
Gasskin <415753928@qq.com>
2026-08-27 20:26:29 +0800

    1

--- Client/Assets/Scripts/Runtime/Porcedure/4.3_BattleCircle.cs
+++ Client/Assets/Scripts/Runtime/Porcedure/4.3_BattleCircle.cs
@@ -2,15 +2,27 @@
 
 namespace Runtime
 {
-    public class BattleCircle: BaseProcedure
+    public class BattleCircle : BaseProcedure
     {
+        private Entity _player;
+        private Entity _enemy;
+        private GameEntry _entry;
+
         public override void Enter(object data = null)
         {
-            
+            _entry = GameEntry.Instance;
+            _player = _entry.EntityManager.GetEntity(_entry.UnitManager.PlayerUid);
+            _enemy = _entry.EntityManager.GetEntity(_entry.UnitManager.EnemyUid);
         }
 
         public override void Update(float dt)
         {
+            if (_player == null || _enemy == null)
+            {
+                return;
+            }
+            
+            
         }
 
         public override void Exit()
